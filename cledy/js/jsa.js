80cccc054baaf5c8dc331aa4883c1638500bf020
leonelpy2001 <leonildobig16@gmail.com>
2024-08-16 10:46:32 +0100

    ENTRADA DE ARQUIVOS

---
+++ cledy/js/jsa.js
@@ -0,0 +1,9 @@
+var precoVariavel = 300
+var desPorcento = 10
+document.querySelector(".precoTotal").innerHTML= precoVariavel / desPorcento + precoVariavel
+document.querySelector(".precoVariavel").innerHTML= precoVariavel
+document.querySelector(".desPr").innerHTML= desPorcento
+
+document.querySelector(".lucro").innerHTML= precoVariavel / desPorcento
+
+      
